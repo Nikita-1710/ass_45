b6250b484ccf2eaeb2957b1d2d1083dda14e3e36
Nikita-1710 <chausalkarnikita@gmail.com>
2025-06-03 14:03:28 +0530

    lucide react installed

---
+++ src/components/ScoreCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function ScoreCard() {
+  return (
+    <div>
+        <h2>
+
+        </h2>
+
+        <h3>
+
+        </h3>
+
+        <div>
+            <Minus />
+            <Plus />
+        </div>
+    </div>
+  )
+}
+
+export default ScoreCard
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,1 +1,26 @@
-import ScoreCard from "../../components/ScoreCard"
+import ScoreCard from "../../components/ScoreCard";
+import toast, { Toaster } from 'react-hot-toast';
+import React, { useState, useEffect } from 'react';
+
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1>Score Board</h1>
+
+        <div>
+
+        </div>
+
+        <div>
+            <button>
+
+            </button>
+        </div>
+        <Toaster />
+    </div>
+  )
+}
+
+export default Home
